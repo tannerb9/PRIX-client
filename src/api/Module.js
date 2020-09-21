f65b9c290d6bdc734971f35d44993bd653a247db
tannerb9 <tanner-brainard@hotmail.com>
2020-09-20 19:02:51 -0500

    chore: removed zombie code

--- src/api/Module.js
+++ src/api/Module.js
@@ -1,11 +1,1 @@
-const remoteURL = "http://localhost:8000";
 
-export default {
-  getIngredients(companyId) {
-    if (isAuthenticated()) {
-      return fetch(
-        `${remoteURL}/ingredient?company=${companyId}`
-      ).then((data) => data.json());
-    }
-  },
-};
