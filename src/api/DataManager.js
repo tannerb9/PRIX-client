0e22e797b127b8bb3a492a84fac0eddc726be188
tannerb9 <tanner-brainard@hotmail.com>
2020-09-22 00:35:30 -0500

    feat: created a fetch call for retrieve actions to the api

--- src/api/DataManager.js
+++ src/api/DataManager.js
@@ -1,6 +1,15 @@
 const remoteURL = "http://localhost:8000";
 
 export default {
+  get(tab, id) {
+    return fetch(`${remoteURL}/${tab}/${id}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Token ${localStorage.getItem("PRIX_token")}`,
+      },
+    }).then((response) => response.json());
+  },
   getAll(tab, companyId) {
     return fetch(`${remoteURL}/${tab}?company=${companyId}`, {
       method: "GET",
