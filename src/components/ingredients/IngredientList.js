0b6ca6baf72280341d30e23812c4ed1c4ce4d20a
tannerb9 <tanner-brainard@hotmail.com>
2020-09-26 23:32:24 -0500

    style: changed IngredientList to reactstrap Table component and moved edit btn to Ingredient component and included it as table column

--- src/components/ingredients/IngredientList.js
+++ src/components/ingredients/IngredientList.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Ingredient from "./Ingredient";
-import EditIngredientForm from "./EditIngredientForm";
 import AddIngredientForm from "./AddIngredientForm";
 import DataManager from "../../api/DataManager";
+import { Table } from "reactstrap";
 
 const IngredientList = (props) => {
   const [ingredients, setIngredients] = useState([]);
@@ -18,24 +18,29 @@ const IngredientList = (props) => {
   return (
     <article className="ingredientList">
       <AddIngredientForm {...props} getIngredients={getIngredients} />
-      {ingredients.map((ingredient, idx) => (
-        <>
+      <Table hover bordered size="sm">
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Category</th>
+            <th>Purchase Quantity</th>
+            <th>Purchase Price</th>
+            <th></th>
+          </tr>
+        </thead>
+        {ingredients.map((ingredient, idx) => (
           <Ingredient
             key={`ingredient--${idx}`}
+            idx={idx}
             ingredient={ingredient}
-          />
-          <EditIngredientForm
-            {...props}
             ingredientCategory={ingredient.ingredient_category}
             ingredientCategoryId={ingredient.ingredient_category_id}
             measurementType={ingredient.measurement_type}
             measurementTypeId={ingredient.measurement_type_id}
             getIngredients={getIngredients}
-            key={`edit-ing--${idx}`}
-            ingredient={ingredient}
           />
-        </>
-      ))}
+        ))}
+      </Table>
     </article>
   );
 };
