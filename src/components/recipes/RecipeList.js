f3e480f5f4b5655999049ff53f778ab591356559
tannerb9 <tanner-brainard@hotmail.com>
2020-09-28 18:30:50 -0500

    refactor: changed RecipeList to Table component and added column headings

--- src/components/recipes/RecipeList.js
+++ src/components/recipes/RecipeList.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Recipe from "./Recipe";
+import AddRecipeForm from "./AddRecipeForm";
 import DataManager from "../../api/DataManager";
+import { Table } from "reactstrap";
 
 const RecipeList = (props) => {
   const [recipes, setRecipes] = useState([]);
@@ -15,9 +17,25 @@ const RecipeList = (props) => {
 
   return (
     <article className="recipeList">
-      {recipes.map((recipe) => (
-        <Recipe key={recipe.id} recipe={recipe} />
-      ))}
+      <AddRecipeForm
+        className="add-btn"
+        {...props}
+        getRecipes={getRecipes}
+      />
+      <Table hover bordered size="sm">
+        <thead>
+          <tr>
+            <th>Recipe</th>
+            <th>Category</th>
+            <th>Servings Per Batch</th>
+            <th>Serving / Batch Sale Price</th>
+            <th></th>
+          </tr>
+        </thead>
+        {recipes.map((recipe) => (
+          <Recipe key={recipe.id} recipe={recipe} />
+        ))}
+      </Table>
     </article>
   );
 };
